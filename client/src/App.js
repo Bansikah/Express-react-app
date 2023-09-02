c294275a88f459179f533c3de2b27b1dfc3718ea
Bansikah <tandapnoelbansikah@gmail.com>
2023-09-02 01:05:49 +0100

    displayed data

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,14 @@ const [backendData, setBackendData] = useState([{}])
 
   return (
     <div>
-
+  
+  {(typeof backendData.users === 'undefined') ? (
+    <p>Loading...</p>
+  ): (
+    backendData.users.map((user, i) => (
+      <p key={i}>{user}</p>
+    ))
+  )}
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({"user":["UserOne","UserTwo","UserThree"]})
+    res.json({"users":["UserOne","UserTwo","UserThree"]})
 })
 
 app.listen(5000, () => {console.log("Server is running on port 5000")})
